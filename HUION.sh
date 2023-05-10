35b3e4bd28bc30e5743c84a9e8f3cedc7fb5675d
l.gazpio <inigo.lopez@ehu.eus>
2023-05-10 14:26:45 +0200

    BUG Fixed in ubuntu? return to initial code

--- HUION.sh
+++ HUION.sh
@@ -41,7 +41,10 @@ screenY=1080
 # map to good screen (dual nvidia)
 # xrandr command to obtain displays
 # there is a nvidia bug -> use HEAD-0 -1 , n instead of output from xrandr
-xsetwacom --set "$tabletstylus" MapToOutput HEAD-0
+
+#xsetwacom --set "$tabletstylus" MapToOutput HEAD-0
+xsetwacom --set "$tabletstylus" MapToOutput HDMI-A-0
+
 # setup ratio :  
 newtabletY=$(( $screenY * $tabletX / $screenX ))  
 xsetwacom --set "$tabletstylus" Area 0 0 "$tabletX" "$newtabletY"  
